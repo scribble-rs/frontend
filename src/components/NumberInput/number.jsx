422839c8fdc5150e16784db6a9b15a34a0a569d9
Marcel Schramm <marceloschr@googlemail.com>
2023-11-05 13:56:16 +0100

    NumberInput now saves value as state
    This will prevent the value being lost on rerenders. This was the case
    when attempting to create a lobby with input that's invalid and caught
    after the request.

--- src/components/NumberInput/number.jsx
+++ src/components/NumberInput/number.jsx
@@ -1,16 +1,20 @@
-import { useRef } from 'preact/hooks';
+import { useRef, useState } from 'preact/hooks';
 import "./number.css"
 
 export function NumberInput(props) {
     const input = useRef(null);
     const stepDown = () => input.current && input.current.stepDown();
     const stepUp = () => input.current && input.current.stepUp();
+    const [value, setValue] = useState(props.value);
 
     return (
         <div class="number-input">
             <button class="number-decrement" onClick={stepDown} type="button">-</button>
             <input size={4} ref={input} type="number" name={props.name}
-                min={props.min} max={props.max} value={props.value} />
+                min={props.min} max={props.max} value={value} onChange={
+                    (event) => {
+                        setValue(event.currentTarget.value);
+                    }} />
             <button class="number-increment" onClick={stepUp} type="button">+</button>
         </div >
     )
